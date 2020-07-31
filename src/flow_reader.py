acd8981bbf341f670580ffe6a1bd5dbadf9cf074
Lukas Jaworski <l.jaworski@umiami.edu>
2020-07-31 12:36:28 -0400

    made a first quick and dirty version of the functional flow meter side, more to come

--- src/flow_reader.py
+++ src/flow_reader.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 from sf04 import *
-from time import sleep
+from time import sleep, time
+from collections import deque
+from statistics import mean
 from os import system
 
 
@@ -9,16 +11,33 @@ with SMBus(3) as bus:
     reset_sensor(bus)
     print("Sensor reset!")
     sleep(0.5)
+    system('clear')
 
     sensor, serial_number, _, _ = read_product_info(bus)
     scale_factor, units, _, _ = read_scale_and_unit(bus)
+    print('Sensor is:')
+    print(sensor)
+    print('Serial Number:')
+    print(serial_number)
+    print('Scale Factor:')
+    print(scale_factor)
+    print('Units:')
+    print(units)
+    sleep(3)
+
     set_read_data(bus)
+    timeout_buffer = deque([], 3000)
     while True:
-        raw_flow_reading, _, crc_result = read_raw_data(bus, True)
+        raw_flow_reading, _, _ = read_raw_data(bus, True)
         scaled_flow_reading = scale_reading(raw_flow_reading, scale_factor)
-        if crc_result:
-            print('The current flow rate is: {} {}'.format(scaled_flow_reading, units))
-        else:
-            print('Error! Error!')
-        sleep(1)
+        print('The current flow rate is: {} {}'.format(scaled_flow_reading, units))
+        if 3100 < scaled_flow_reading < 3500:
+            timeout_buffer.append((time(), scaled_flow_reading))
+            beggining_average = mean(list(timeout_buffer)[:100])
+            ending_average = mean(list(timeout_buffer)[-100:])
+            if abs(beggining_average-ending_average) < 100 and len(timeout_buffer) > 2500:
+                print('ALL DONE!!')
+            else:
+                print('Approaching the end')
+        sleep(0.25)
         system('clear')
