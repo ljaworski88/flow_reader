cc04d23a377e1af2424eb29e57b2dc445b71034a
Lukas Jaworski <ljaworski88@gmail.com>
2020-09-14 18:08:39 -0400

    updated install script

--- install.sh
+++ install.sh
@@ -1,17 +1,33 @@
-#!usr/bin/env/ bash
+#!/bin/bash
 
-apt install python3-pyvisa python3-pyvisa-py python3-sklearn python3-yaml
-apt install python3-pyqt5 python3-numpy python3-serial python3-pyqtgraph
+# Back-up the location of the starting directory
+STARTING_DIR=$PWD
+
+# Turn on the two needed I2C buses
+sudo echo 'dtparam=i2c_arm=on' >> /boot/config.txt
+sudo echo 'dtoverlay=i2c-gpio,bus=3' >> /boot/config.txt
+
+# Install python dependencies
+sudo apt install python3-pyvisa python3-pyvisa-py python3-numpy python3-sklearn
+sudo apt install python3-pyqt5 python3-yaml python3-serial python3-pyqtgraph
+
+# Install the NAU7802 library
 cd ~
 git clone https://github.com/ljaworski88/nau7802-py.git
-rm /boot/overlays/i2c-gpio.dtbo
 cd nau7802-py
-cp i2c-gpio.dtbo /boot/overlays/i2c-gpio.dtbo
 pip3 install .
-echo 'dtparam=i2c_arm=on' >> /boot/config.txt
-echo 'dtoverlay=i2c-gpio,bus=3' >> /boot/config.txt
+
+# Install the SF04 sensor library ot control the Sensirion SLG-150 flow sensor
 cd ~
 git clone https://github.com/ljaworski88/sensirion-sf04-python.git
 cd sensirion-sf04-python
+sudo rm /boot/overlays/i2c-gpio.dtbo
+sudo cp i2c-gpio.dtbo /boot/overlays/i2c-gpio.dtbo
 pip3 install .
 
+# Place a symbolic link to the flow reader on the desktop
+cd ~/Desktop
+ln -s home/pi/flow_reader/src/python/flow_reader.py
+
+# Return to the install directory
+cd $STARTING_DIR
